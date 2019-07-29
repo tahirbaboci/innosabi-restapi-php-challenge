40285630f7b96f51cbad55ee5e8ec0499af33559
iribaboci <tahirbaboci96@gmail.com>
2019-07-29 22:34:52 +0200

    initalized highlighting task

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -19,11 +19,14 @@
 
         }
 
-        public function highlight(){
+        public function highlight($searchString, $theTextString){
             // get the words in sequence from textbox
+            $searchString = preg_replace("#[\s]+#", " ", $searchString);
+            $searchStringWords = explode(" ", $searchString);
             
             // get the words in sequence from TEXT
-            
+            $theTextString = preg_replace("#[\s]+#", " ", $theTextString);
+            $textStringWords = explode(" ", $theTextString);
             // search for the first word in text
                     // if found { see the next word after the one who was found and if true repeat }
                     // if Notfound {search for the next word}
--- public/index.php
+++ public/index.php
@@ -2,5 +2,11 @@
 
   require_once '../app/bootstrap.php';
 
+  /*
+  echo '<pre>';
+  var_dump($_SERVER);
+  echo '</pre>';
+  */
+  
   // Init Core Library
   $init = new Core;
